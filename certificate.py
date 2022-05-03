38fdca81ae6319544b0e9ce8d340ed265018f6ce
Mehul Ved <mehul.n.ved@colearn.id>
2022-05-03 19:36:44 +0530

    Refactored to a more manageable code

--- certificate.py
+++ certificate.py
@@ -1,22 +1,53 @@
 #!/usr/bin/env python
+import logging
+logger = logging.getLogger(__name__)
+BLACK = (0, 0, 0)
 
-def fill(data, template, uuid, cert_dir):
+
+def read_config():
     import configparser
-    import logging
-    from datetime import datetime
-    from PIL import Image
-    from PIL import ImageFont
-    from PIL import ImageDraw
-
     config = configparser.ConfigParser()
     config.read('config.ini')
     config_dump = {}
     for each_section in config.sections():
         for (each_key, each_val) in config.items(each_section):
             config_dump[each_key] = each_val
-    logging.debug({"config": config_dump})
+    logger.debug({"config": config_dump})
+    return config
 
-    logging.info({
+
+def get_dimensions(*, image, font, item, config, key):
+    image_width, image_height = image.size
+    logger.info({
+        "image": {
+            "width": image_width,
+            "height": image_height
+        }
+    })
+    canvas_width, canvas_height = font.getsize(item)
+    try:
+        item_width = config.getint(key, 'width', fallback=0)
+    except KeyError:
+        item_canvas_center = int((image_width - canvas_width) / 2)
+        item_left_offset = config.getint(key, 'width_offset_left', fallback=0)
+        item_right_offset = config.getint(key, 'width_offset_right', fallback=0)
+        item_width = item_left_offset + item_canvas_center - item_right_offset
+    try:
+        item_height = int(config.get(key, 'height'))
+    except KeyError:
+        raise
+    dimensions = (item_width, item_height)
+    return dimensions
+
+
+def fill(data, template, uuid, cert_dir):
+    from datetime import datetime
+    from PIL import Image
+    from PIL import ImageFont
+    from PIL import ImageDraw
+
+    config = read_config()
+    logger.info({
         "data": data,
     })
     img = Image.open(template)
@@ -24,44 +55,31 @@ def fill(data, template, uuid, cert_dir):
     # Credits:
     # https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
 
-    image_width, image_height = img.size
-    logging.info({
-        "image": {
-            "width": image_width,
-            "height": image_height
-        }
-    })
     for key, value in data.items():
-        logging.info({
+        logger.info({
             key: value
         })
         item = data[key].strip()
         font_size = config.getint(key, 'font_size')
         font = ImageFont.truetype(r'./news-serif.ttf', font_size)
-        w, h = font.getsize(item)
-        try:
-            item_width = config.getint(key, 'width', fallback=0)
-        except KeyError:
-            item_canvas_center = int((image_width-w)/2)
-            item_left_offset = config.getint(key, 'width_offset_left', fallback=0)
-            item_right_offset = config.getint(key, 'width_offset_right', fallback=0)
-            item_width = item_left_offset + item_canvas_center - item_right_offset
-        item_height = int(config.get(key, 'height'))
-        draw.text((item_width, item_height), item, (0, 0, 0), font=font)
+
+        item_dimensions = get_dimensions(image=img, font=font, item=item, config=config, key=key)
+        item_colour = BLACK
+        draw.text(item_dimensions, item, item_colour, font=font)
 
     cert_name = data['name'].lower().replace(' ', '_')
     cert_path = f'{cert_dir}/{cert_name}.jpg'
     try:
         img.save(cert_path)
         img.close()
-        logging.info({
+        logger.info({
             "status": "saved",
             "certificate": cert_path,
             "identifier": uuid,
             "time": str(datetime.now())
         })
     except FileNotFoundError:
-        logging.error({
+        logger.error({
             "status": "error",
             "exception": "Unable to save file. Please check the given directory exists.",
             "identifier": uuid,
--- fill_certificate.py
+++ fill_certificate.py
@@ -7,50 +7,68 @@ from datetime import datetime
 import uuid
 from certificate import fill
 
+
 logging.basicConfig(level=logging.DEBUG)
-run_id = str(uuid.uuid4())
-logging.info({
-    "status": "started",
-    "identifier": run_id,
-    "time": str(datetime.now())
-})
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--datafile", help="Pass optional file path. Default: data/timesheet.csv")
-parser.add_argument("--outputpath", help="Pass optional output path. Default: certs/")
-parser.add_argument("--certificatefile", help="Pass optional certificate file. Default: ./certificate-template.jpg")
-args = parser.parse_args()
-
-if args.datafile:
-    filepath = args.datafile
-else:
-    filepath = f"data/timesheet.csv"
-
-if args.outputpath:
-    output_path = args.outputpath
-else:
-    output_path = "certs"
-
-if args.certificatefile:
-    certificate_template = args.certificatefile
-else:
-    certificate_template = "./certificate-template.jpg"
-
-logging.info({
-    "timesheet": filepath,
-    "certificate_directory": output_path,
-    "certificate_template": certificate_template
-})
+logger = logging.getLogger(__name__)
 
 
-with open(filepath) as csvfile:
-    tsreader = csv.DictReader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    for row in tsreader:
-        logging.info({"data": row, "time": str(datetime.now())})
-        fill(row, certificate_template, run_id, output_path)
+def setup_paths():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datafile", help="Pass optional file path. Default: data/timesheet.csv")
+    parser.add_argument("--outputpath", help="Pass optional output path. Default: certs/")
+    parser.add_argument("--certificatefile", help="Pass optional certificate file. Default: ./certificate-template.jpg")
+    args = parser.parse_args()
 
-logging.info({
-    "status": "completed",
-    "identifier": run_id,
-    "time": str(datetime.now())
-})
+    if args.datafile:
+        filepath = args.datafile
+    else:
+        filepath = f"data/timesheet.csv"
+
+    if args.outputpath:
+        cert_path = args.outputpath
+    else:
+        cert_path = "certs"
+
+    if args.certificatefile:
+        template = args.certificatefile
+    else:
+        template = "./certificate-template.jpg"
+
+    logger.info({
+        "timesheet": filepath,
+        "certificate_directory": cert_path,
+        "certificate_template": template
+    })
+    path_list = {
+        "timesheet": filepath,
+        "cert_path": cert_path,
+        "template": template
+    }
+    return path_list
+
+
+if __name__ == "__main__":
+    # Logging
+    run_id = str(uuid.uuid4())
+    logger.info({
+        "status": "started",
+        "identifier": run_id,
+        "time": str(datetime.now())
+    })
+    # Paths
+    paths = setup_paths()
+    timesheet = paths["timesheet"]
+    certificate_template = paths["template"]
+    output_path = paths["cert_path"]
+    # Read data
+    with open(timesheet) as csvfile:
+        tsreader = csv.DictReader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in tsreader:
+            logger.info({"data": row, "time": str(datetime.now())})
+            fill(row, certificate_template, run_id, output_path)
+    # Finish
+    logger.info({
+        "status": "completed",
+        "identifier": run_id,
+        "time": str(datetime.now())
+    })
