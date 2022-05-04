a9e885089d134e3775ce448ee26e681dd06202e6
Mehul Ved <mehul.n.ved@colearn.id>
2022-05-04 21:24:34 +0530

    Docstrings

--- app.py
+++ app.py
@@ -34,12 +34,14 @@ project_list = [{
 
 @app.get("/projects")
 def projects():
+    """List all projects"""
     app.logger.info({"event": "All Projects Listed"})
     return jsonify(project_list)
 
 
 @app.get("/projects/<int:project_id>")
 def project(project_id):
+    """Get a specified project, by project id"""
     app.logger.info({"event": f"Project {project_id} listed"})
     for item in range(len(project_list)):
         if project_list[item]["id"] == project_id:
@@ -50,6 +52,7 @@ def project(project_id):
 
 @app.post("/projects")
 def create_project():
+    """"Create a new project"""
     app.logger.info({"request": request.form.get('project_name')})
     if request.form['project_name']:
         project_name = json.loads(request.form.get('project_name'))
@@ -62,15 +65,16 @@ def create_project():
 
 @app.put("/projects/<int:project_id>")
 def update_project(project_id):
+    """Update a project, specified by project id"""
     app.logger.info({"request": request.form.get('project_name')})
     if request.form['project_name']:
         project_name = json.loads(request.form.get('project_name'))
         for item in range(len(project_list)):
             if project_list[item]["id"] == project_id:
-                project_list[item]['name']=project_name
-            else:
-                res = make_response("Project ID is incorrect", 400)
-        return jsonify(project_list)
+                project_list[item]['name'] = project_name
+                return jsonify(project_list)
+        res = make_response("Project ID is incorrect", 400)
+        return res
     else:
         res = make_response("Project Name cannot be empty", 400)
         return res
