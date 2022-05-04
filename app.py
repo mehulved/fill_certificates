c8bd80408eaf6005f1abc657cb5d5f7edee1f879
Mehul Ved <mehul.n.ved@colearn.id>
2022-05-04 20:25:08 +0530

    Added logging

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 
 from flask import Flask, jsonify, make_response
+from logging.config import dictConfig
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 app = Flask(__name__)
+
+
 project_list = [{
     "id": 1,
     "name": "Project 1"
@@ -15,14 +33,14 @@ project_list = [{
 
 @app.get("/projects")
 def projects():
+    app.logger.info({"event": "All Projects Listed"})
     return jsonify(project_list)
 
 
 @app.get("/projects/<int:project_id>")
 def project(project_id):
+    app.logger.info({"event": f"Project {project_id} listed"})
     for item in range(len(project_list)):
-        print(type(project_list[item]))
-        print(project_list[item])
         if project_list[item]["id"] == project_id:
             return jsonify(project_list[item])
     res = make_response("", 204)
